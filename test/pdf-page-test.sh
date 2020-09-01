923b05a0cb84b3cc3ae31540cfdd2d5f2d6c6afd
John Mikael Gundersen <john.gundersen@evry.com>
2020-09-01 08:19:13 +0200

    Lets try pdfinfo then

--- test/pdf-page-test.sh
+++ test/pdf-page-test.sh
@@ -2,7 +2,7 @@
 cd output;
 echo $(pwd)
 echo $(ls)
-NUM_OF_PAGES=$(exiftool -T -filename -PageCount -s3 -ext pdf resume.pdf);
+NUM_OF_PAGES=$(pdfinfo foo.pdf | grep 'Pages' | awk '{print $2}');
 if (( $NUM_OF_PAGES > 1 )); then
     return 1;
 fi
