0dfce234a40be5574a79b3d68a579f1b418e3c3d
John Mikael Gundersen <john.gundersen@evry.com>
2020-09-01 08:56:01 +0200

    everting if

--- test/pdf-page-test.sh
+++ test/pdf-page-test.sh
@@ -4,7 +4,7 @@ echo $(pwd);
 echo $(ls);
 NUM_OF_PAGES=$(pdfinfo resume.pdf | grep 'Pages' | awk '{print $2}');
 echo "Num of pages found $NUM_OF_PAGES";
-if [$NUM_OF_PAGES -gt 1]; then
+if (($NUM_OF_PAGES > 1)); then
     echo "Too many pages ($NUM_OF_PAGES)";
     exit 1;
 fi
