c70f8620c946da12e195f29fd92b4ae3a0702fad
John Mikael Gundersen <john.gundersen@evry.com>
2020-09-01 08:47:50 +0200

    Bash scripting is hard

--- test/pdf-page-test.sh
+++ test/pdf-page-test.sh
@@ -3,7 +3,7 @@ cd output;
 echo $(pwd);
 echo $(ls);
 NUM_OF_PAGES=$(pdfinfo resume.pdf | grep 'Pages' | awk '{print $2}');
-echo 'Num of pages found';
+echo "Num of pages found $NUM_OF_PAGES";
 if (( $NUM_OF_PAGES > 1 )); then
     echo "Too many pages ($NUM_OF_PAGES)";
     return 1;
