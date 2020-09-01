43062a7b82a94ff373c4926bd5b20aa9d5cdad12
John Mikael Gundersen <john.gundersen@evry.com>
2020-09-01 08:26:09 +0200

    Correct pdf

--- test/pdf-page-test.sh
+++ test/pdf-page-test.sh
@@ -2,7 +2,7 @@
 cd output;
 echo $(pwd)
 echo $(ls)
-NUM_OF_PAGES=$(pdfinfo foo.pdf | grep 'Pages' | awk '{print $2}');
+NUM_OF_PAGES=$(pdfinfo resume.pdf | grep 'Pages' | awk '{print $2}');
 if (( $NUM_OF_PAGES > 1 )); then
     return 1;
 fi
