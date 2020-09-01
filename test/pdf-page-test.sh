ce5919ce20510becdcb4511a2dc413c185ac5dd1
John Mikael Gundersen <john@jmgundersen.com>
2020-09-01 09:12:48 +0200

    Add pdf tests (#2)
    * My resume needs tests
    
    Co-authored-by: John Mikael Gundersen <john.gundersen@evry.com>

---
+++ test/pdf-page-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd output;
+NUM_OF_PAGES=$(pdfinfo resume.pdf | grep 'Pages' | awk '{print $2}');
+echo "Num of pages found $NUM_OF_PAGES";
+if (($NUM_OF_PAGES > 1)); then
+    echo "Too many pages ($NUM_OF_PAGES)";
+    exit 1;
+fi
+exit 0;
