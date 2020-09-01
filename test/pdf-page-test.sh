5051d0610d9bca1492ebbf4a9c972c3d7d63e77c
John Mikael Gundersen <john.gundersen@evry.com>
2020-09-01 08:37:11 +0200

    is there a different issue maybe

--- test/pdf-page-test.sh
+++ test/pdf-page-test.sh
@@ -3,7 +3,9 @@ cd output;
 echo $(pwd)
 echo $(ls)
 NUM_OF_PAGES=$(pdfinfo resume.pdf | grep 'Pages' | awk '{print $2}');
+echo 'Num of pages found';
 if (( $NUM_OF_PAGES > 1 )); then
+    echo "Too many pages ($NUM_OF_PAGES)";
     return 1;
 fi
 return 0;
