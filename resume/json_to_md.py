58c05b700a00ff1f724441fbc5a118bba2dcd2fa
John Mikael Gundersen <john@jmgundersen.com>
2021-08-15 19:27:25 +0200

    Working on translating json to markdown

--- resume/json_to_md.py
+++ resume/json_to_md.py
@@ -1,10 +1,12 @@
 import json
 import os
+from datetime import datetime
 from typing import List
 
-from resume.resume_types import Basics, Resume, resume_from_dict
+from resume.resume_types import Basics, Resume, resume_from_dict, Volunteer, Education
 
 NEW_LINE = ''
+DASH_LINE = ''
 
 
 def get_contact_and_social_line(basics: Basics) -> List[str]:
@@ -25,9 +27,70 @@ def get_header_information(basics: Basics) -> List[str]:
 def convert_resume(resume: Resume) -> str:
     content = [
         get_header_information(resume.basics),
-        get_contact_and_social_line(resume.basics)
+        get_contact_and_social_line(resume.basics),
+        convert_volunteers("Experience", resume.work),
+        convert_educations(resume.education)
     ]
     return os.linesep.join([item_line for category in content for item_line in category])
+
+
+def parse_date(date: datetime, default: str = None) -> str:
+    if not date and not default:
+        raise TypeError("Missing date")
+    if not date:
+        return default
+    return date.strftime('%m.%Y')
+
+
+def convert_education(education: Education) -> List[str]:
+    start_date = parse_date(education.start_date)
+    end_date = parse_date(education.end_date, "Current")
+    content = [
+        f'**{education.study_type}, {education.area}**, {education.institution}({start_date} - {end_date})'
+    ]
+    for course in education.courses:
+        content.append(f' * {course}')
+    return content
+
+
+def convert_educations(educations: List[Education]) -> List[str]:
+    if len(educations) == 0:
+        return []
+    content = [
+        "Education",
+        DASH_LINE
+    ]
+    for education in educations:
+        for line in convert_education(education):
+            content.append(line)
+        content.append(NEW_LINE)
+
+
+def convert_volunteer(volunteer: Volunteer) -> List[str]:
+    start_date = parse_date(volunteer.start_date)
+    end_date = parse_date(volunteer.end_date, "Current")
+    content = [
+        f'**{volunteer.position}**, {volunteer.name}({start_date} - {end_date})',
+        NEW_LINE,
+        volunteer.summary,
+        NEW_LINE
+    ]
+    [content.append(f' * {highlight}') for highlight in volunteer.highlights]
+    return content
+
+
+def convert_volunteers(title: str, volunteers: List[Volunteer]) -> List[str]:
+    if len(volunteers) == 0:
+        return []
+    content = [
+        title,
+        DASH_LINE
+    ]
+    for volunteer in volunteers:
+        for line in convert_volunteer(volunteer):
+            content.append(line)
+        content.append(NEW_LINE)
+    return content
 
 
 def do_conversion(source: str, dest: str):
@@ -36,7 +99,3 @@ def do_conversion(source: str, dest: str):
     markdown = convert_resume(resume)
     with open(dest, 'w') as f:
         f.write(markdown)
-
-
-class JsonToMarkDownConverter:
-    pass
--- tests/test_json_to_md.py
+++ tests/test_json_to_md.py
@@ -1,32 +1,112 @@
+import datetime
 from typing import List
 from unittest import TestCase
 
-from resume.json_to_md import get_header_information
-from resume.resume_types import Basics, Location, Profile
+from parameterized import parameterized
+
+from resume.json_to_md import get_header_information, parse_date, convert_volunteer, convert_education
+from resume.resume_types import Basics, Location, Profile, Volunteer, Education
 
 
 class Test(TestCase):
     def test_get_header_information_verify_name(self):
         result = get_header_information(create_basics())
         self.assertEqual(result[0], 'Tom Waits')
-        self.assertEqual(result[1], '===================')
-        self.assertEqual(result[2], '')
-        self.assertEqual(result[3], '#### Singer/songwriter/musician/composer/actor')
 
     def test_get_header_information_verify_title_line(self):
         result = get_header_information(create_basics())
         self.assertEqual(result[1], '===================')
-        self.assertEqual(result[2], '')
-        self.assertEqual(result[3], '#### Singer/songwriter/musician/composer/actor')
 
     def test_get_header_information_verify_newline_string(self):
         result = get_header_information(create_basics())
         self.assertEqual(result[2], '')
-        self.assertEqual(result[3], '#### Singer/songwriter/musician/composer/actor')
 
     def test_get_header_information_verify_label(self):
         result = get_header_information(create_basics())
         self.assertEqual(result[3], '#### Singer/songwriter/musician/composer/actor')
+
+    def test_parse_date_missing_date_and_default_error_should_be_thrown(self):
+        with self.assertRaises(Exception) as context:
+            parse_date(None, None)
+        self.assertTrue('Missing date' in str(context.exception))
+
+    def test_parse_date_missing_date_and_provided_default_should_be_returned(self):
+        self.assertEqual(parse_date(None, 'Current'), 'Current')
+
+    def test_parse_date_actual_date_provided_should_be_returned_in_proper_string_form(self):
+        self.assertEqual(parse_date(datetime.datetime(1988, 6, 21), 'Current'), '06.1988')
+
+    def test_convert_volunteer_verify_title_line(self):
+        self.assertEqual(convert_volunteer(create_volunteer())[0], '**Iron man**, The avengers(06.1988 - Current)')
+
+    @parameterized.expand([[1], [3]])
+    def test_convert_volunteer_verify_empty_lines(self, index: int):
+        result = convert_volunteer(create_volunteer())
+        self.assertEqual(result[index], '')
+
+    def test_convert_volunteer_verify_summary_line(self):
+        result = convert_volunteer(create_volunteer())
+        self.assertEqual(result[2], 'Worked with a group that battles horrors that might threaten earth')
+
+    @parameterized.expand([
+        [4, ' * Fought Ultron'],
+        [5, ' * Been in close combat with the Hulk'],
+        [6, ' * Met literal norse gods'],
+        [7, ' * Married'],
+        [8, ' * Been pretty cool to Spiderman']
+    ])
+    def test_convert_volunteer_verify_highlights(self, index: int, expected_result: str):
+        result = convert_volunteer(create_volunteer())
+        self.assertEqual(result[index], expected_result)
+
+    def test_convert_education_verify_title_line(self):
+        result = convert_education(create_education())
+        self.assertTrue(result[0], '**Bachelor, Cool Course**, Awesome University (03.420 - 04.1337)')
+
+    @parameterized.expand([
+        [1, ' * Being chill: The fundamentals'],
+        [2, ' * Swag 101'],
+        [3, ' * Dabbing']
+    ])
+    def test_convert_education_verify_courses(self, index: int, expected_result: str):
+        result = convert_education(create_education())
+        self.assertTrue(result[index], expected_result)
+
+
+def create_education() -> Education:
+    return Education(
+        institution='Awesome university',
+        url='awesomeuniversity.com',
+        area='Cool Course',
+        study_type='Bachelors',
+        start_date=datetime.datetime(420, 3, 21),
+        end_date=datetime.datetime(1337, 4, 21),
+        score='N/A',
+        courses= [
+            'Being chill: The fundamentals',
+            'Swag 101',
+            'Dabbing'
+        ]
+    )
+
+
+def create_volunteer() -> Volunteer:
+    return Volunteer(
+        name='The avengers',
+        position='Iron man',
+        url='https://en.wikipedia.org/wiki/The_Avengers_(2012_film)',
+        start_date=datetime.datetime(1988, 6, 21),
+        end_date=None,
+        summary='Worked with a group that battles horrors that might threaten earth',
+        highlights=[
+            'Fought Ultron',
+            'Been in close combat with the Hulk',
+            'Met literal norse gods',
+            'Married',
+            'Been pretty cool to Spiderman'
+        ],
+        organization=None
+    )
 
 
 def create_basics() -> Basics:
