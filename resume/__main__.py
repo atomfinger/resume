010578065eb335ab3e0165645d10c3539e088876
John Mikael Gundersen <john@jmgundersen.com>
2021-08-16 19:38:48 +0200

    - Adding yaml

--- resume/__main__.py
+++ resume/__main__.py
@@ -1,8 +1,10 @@
 import argparse
 import json
 from argparse import Namespace
+from typing import Optional
 
 from resume.json_to_md import convert_to_markdown
+from resume.json_to_yaml import convert_to_yaml
 from resume.resume_types import resume_from_dict
 
 
@@ -10,20 +12,24 @@ def parse_arguments() -> Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', metavar='path', type=str, help='Input JSON file', required=True)
     parser.add_argument('--output', metavar='path', type=str, help='Output file', required=True)
-    parser.add_argument('--format', type=str, choices=['markdown'], required=True)
+    parser.add_argument('--format', type=str, choices=['markdown', 'yaml'], required=True)
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
+    new_file_content: Optional[str] = None
     with open(args.input, 'r') as f:
         resume = resume_from_dict(json.loads(f.read()))
     if args.format.lower() == 'markdown':
         new_file_content = convert_to_markdown(resume)
+    if args.format.lower()  == 'yaml':
+        convert_to_yaml(resume, args.output)
     else:
         raise ValueError(f'{args.format} is not a supported format')
-    with open(args.output, 'w+') as f:
-        f.write(new_file_content)
+    if new_file_content:
+        with open(args.output, 'w+') as f:
+            f.write(new_file_content)
 
 
 if __name__ == '__main__':
