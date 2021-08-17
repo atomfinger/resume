cc7cc47287be3639fe733455d57a9c6cfd8f321a
John Mikael Gundersen <john@jmgundersen.com>
2021-08-18 00:47:05 +0200

    - Removing XML schema. It became a hassle.

--- resume/json_to_xml.py
+++ resume/json_to_xml.py
@@ -1,5 +1,3 @@
-from lxml import etree
-
 import xmltodict
 
 from resume.resume_types import Resume
@@ -14,9 +12,3 @@ def convert_to_xml(resume: Resume, output: str):
     data = {'resume': data}
     with open(output, 'w+') as f:
         f.write(xmltodict.unparse(data))
-    xmlschema_doc = etree.parse('./schemas/resume-schema.xsd')
-    xmlschema = etree.XMLSchema(xmlschema_doc)
-    xml_doc = etree.parse(output)
-    if not xmlschema.validate(xml_doc):
-        raise InvalidFileStructureError('XML does not match provided schema')
-
