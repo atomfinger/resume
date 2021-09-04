11718417c8b0ee36855119abeb2a6b2b40b1e81a
John Mikael Gundersen <john@jmgundersen.com>
2021-09-04 17:25:58 +0200

    Adding Epub generation

--- resume/html_to_epub.py
+++ resume/html_to_epub.py
@@ -1,9 +1,8 @@
-from pypub.chapter import create_chapter_from_file
-from pypub.epub import Epub
+import pypub
 
 
 def convert_to_epub(origin: str, output: str):
-    epub = Epub('Resume')
-    chapter = create_chapter_from_file(origin)
+    epub = pypub.Epub('Resume')
+    chapter = pypub.create_chapter_from_file(origin)
     epub.add_chapter(chapter)
     epub.create_epub(output)
